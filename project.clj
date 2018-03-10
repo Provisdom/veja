3e503eb470696850571d50cc36aef3b49e5d49ff
sparkofreason <dave.d.dixon@gmail.com>
2018-03-10 12:34:13 -0800

    Add scm to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject provisdom/veja "0.1.0"
   :description "Clojure library to display Vega and Vega-Lite charts in JupyterLab"
   :url "https://github.com/Provisdom/veja"
+  :scm {:url "https://github.com/Provisdom/veja"}
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
